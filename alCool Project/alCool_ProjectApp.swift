44bae95047cfb2a91bb6819721c43a8cabbc91b6
PierSic-dev <113537165+PierSic-dev@users.noreply.github.com>
2022-10-22 22:33:38 +0200

    Initial Commit

---
+++ alCool Project/alCool_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  alCool_ProjectApp.swift
+//  alCool Project
+//
+//  Created by Pierpaolo Siciliano on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct alCool_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

458980f5afa9d2009bdb04eee87d7d4d9ad13e2f
Arturo Alfani <aalfani22@fed.idserve.net>
2022-11-01 16:20:15 +0100

    Classe story resa conforme a Codable

--- alCool Project/Data/DataModel.swift
+++ alCool Project/Data/DataModel.swift
@@ -15,16 +15,37 @@ struct Enigma: Identifiable, Codable {
     let solution: String
 }
 
-class Story: ObservableObject {
+class Story: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name, tokens, enigmas
+    }
+    
     @Published var name: String
     @Published var tokens: Int
     @Published var enigmas = [Enigma]()
     
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(tokens, forKey: .tokens)
+        try container.encode(enigmas, forKey: .enigmas)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        name = try container.decode(String.self, forKey: .name)
+        tokens = try container.decode(Int.self, forKey: .tokens)
+        enigmas = try container.decode([Enigma].self, forKey: .enigmas)
+    }
+
     init(name: String, tokens: Int) {
         self.name = name
         self.tokens = tokens
         enigmas = []
     }
+
 }
 
 class Stories: ObservableObject {
